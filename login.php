f03db8bb62aa8849bcc235c0100864ae664713a9
5ly38 <>
2017-10-17 15:55:23 +0200

    v1

---
+++ login.php
@@ -0,0 +1,25 @@
+<body>
+    <a href="film_ajouter.php"><center>Ajouter Film</center></a>
+    <br>
+    <form action="login.php" method="post"
+        <label><h1>Menu Utilisateur</h1></label>
+        <br>
+        <br>
+        <br>
+        <label>Login :</label>
+        <textarea name="identifiant"></textarea>
+        <br>
+        <label>Mot de passe : </label>
+        <textarea name="mpd"></textarea>
+        <center><input type="submit" value="Valider"/></center>
+    </form>
+</body>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
